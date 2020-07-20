e837d9bd6bfaac836b865c6dd1f28c9d3fd6f6ca
David Heizer <DavidHeizer@QuickenLoans.com>
2020-07-20 17:50:28 -0400

    Final product with bells and whistles

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace DiceRolling
             bool possible = false;
             int roll1 = 0;
             int roll2 = 0;
-            bool proceed = true;
+            int proceed = 1;
 
             Console.WriteLine("Welcome to the Grand Circus Casino!");
 
@@ -30,15 +30,28 @@ namespace DiceRolling
 
                 roll1 = RollDie(sides);
                 roll2 = RollDie(sides);
-                Console.WriteLine($"You rolled a {roll1} and a {roll2}.");
-                //DisplayDice();
-                if (sides == 6)
+                int total = roll1 + roll2;
+                if (sides == 2)
                 {
+                    CoinFlip(roll1, roll2);
+                }
+                else if (sides == 6)
+                {
+                    DisplayDice(roll1, roll2);
+                    Console.WriteLine($"You rolled a {roll1} and a {roll2} ({total} total)");
                     CheckResult(roll1, roll2);
+                }
+                else
+                {
+                    Console.WriteLine($"You rolled a {roll1} and a {roll2} ({total} total)");
                 }
                 proceed = PlayAgain();
                 Console.WriteLine();
-            } while (proceed);
+                if (proceed == 2)
+                {
+                    possible = false;
+                }
+            } while (proceed!=0);
             
 
 
@@ -130,25 +143,31 @@ namespace DiceRolling
             }
         }
 
-        static bool PlayAgain()
+        static int PlayAgain()
         {
-            bool proceed = false;
+            //Returns 0 if not playing again, Returns 1 if playing again, returns 2 if needing to change dice
+            int proceed=3;  
             bool isValid;
 
             do
             {
-                Console.Write("Roll again? (y/n): ");
+                Console.Write("Roll again or change die? (y/n/change): ");
                 string input = Console.ReadLine();
                 input = input.ToUpper();
                 if (input == "Y" || input == "YES")
                 {
-                    proceed = true;
+                    proceed = 1;
                     isValid = true;
                 }
                 else if (input == "N" || input == "NO")
                 {
                     Console.WriteLine("Goodbye!");
-                    proceed = false;
+                    proceed = 0;
+                    isValid = true;
+                }
+                else if(input == "CHANGE")
+                {
+                    proceed = 2;
                     isValid = true;
                 }
                 else
@@ -159,6 +178,198 @@ namespace DiceRolling
             } while (!isValid);
 
             return proceed;
+        }
+
+        static void DisplayDice(int die1, int die2)
+        {
+            Console.WriteLine(" ___   ___");
+            // Top dots
+            if (die1 == 6 || die1 == 5 || die1 == 4)
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{TwoDot()} {TwoDot()}");
+                }
+                else if (die2 == 1)
+                {
+                    Console.WriteLine($"{TwoDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{TwoDot()} {RightDot()}");
+                }
+            }
+            else if (die1 == 1)
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{NoDot()} {TwoDot()}");
+                }
+                else if (die2 == 1)
+                {
+                    Console.WriteLine($"{NoDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{NoDot()} {RightDot()}");
+                }
+            }
+            else
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{RightDot()} {TwoDot()}");
+                }
+                else if(die2 == 1)
+                {
+                    Console.WriteLine($"{RightDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{RightDot()} {RightDot()}");
+                }
+            }
+            //Middle Dots
+            if (die1 == 5 || die1 == 3 || die1 == 1)
+            {
+                if (die2 == 5 || die2 == 3 || die2 == 1)
+                {
+                    Console.WriteLine($"{MidDot()} {MidDot()}");
+                }
+                else if (die2 == 6)
+                {
+                    Console.WriteLine($"{MidDot()} {TwoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{MidDot()} {NoDot()}");
+                }
+            }
+            else if (die1 == 6)
+            {
+                if (die2 == 5 || die2 == 3 || die2 == 1)
+                {
+                    Console.WriteLine($"{TwoDot()} {MidDot()}");
+                }
+                else if (die2 == 6)
+                {
+                    Console.WriteLine($"{TwoDot()} {TwoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{TwoDot()} {NoDot()}");
+                }
+            }
+            else
+            {
+                if (die2 == 5 || die2 == 3 || die2 == 1)
+                {
+                    Console.WriteLine($"{NoDot()} {MidDot()}");
+                }
+                else if (die2 == 6)
+                {
+                    Console.WriteLine($"{NoDot()} {TwoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{NoDot()} {NoDot()}");
+                }
+            }
+            // Lower Dots
+            if (die1 == 6 || die1 == 5 || die1 == 4)
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{TwoDot()} {TwoDot()}");
+                }
+                else if (die2 == 1)
+                {
+                    Console.WriteLine($"{TwoDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{TwoDot()} {LeftDot()}");
+                }
+            }
+            else if (die1 == 1)
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{NoDot()} {TwoDot()}");
+                }
+                else if (die2 == 1)
+                {
+                    Console.WriteLine($"{NoDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{NoDot()} {LeftDot()}");
+                }
+            }
+            else
+            {
+                if (die2 == 6 || die2 == 5 || die2 == 4)
+                {
+                    Console.WriteLine($"{LeftDot()} {TwoDot()}");
+                }
+                else if (die2 == 1)
+                {
+                    Console.WriteLine($"{LeftDot()} {NoDot()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{LeftDot()} {LeftDot()}");
+                }
+            }
+            Console.WriteLine(" ˉˉˉ   ˉˉˉ");
+        }
+
+        static string TwoDot()
+        {
+            string dotdot = "|o o|";
+            return dotdot;
+        }
+        static string RightDot()
+        {
+            string dotdot = "|  o|";
+            return dotdot;
+        }
+        static string MidDot()
+        {
+            string dotdot = "| o |";
+            return dotdot;
+        }
+        static string LeftDot()
+        {
+            string dotdot = "|o  |";
+            return dotdot;
+        }
+        static string NoDot()
+        {
+            string dotdot = "|   |";
+            return dotdot;
+        }
+
+        static void CoinFlip(int coin1, int coin2)
+        {
+            if (coin1 == 1)
+            {
+                Console.Write("Heads and ");
+            }
+            else
+            {
+                Console.Write("Tails and ");
+            }
+            if (coin2 == 1)
+            {
+                Console.WriteLine("Heads");
+            }
+            else
+            {
+                Console.WriteLine("Tails");
+            }
+               
+
         }
 
     }
