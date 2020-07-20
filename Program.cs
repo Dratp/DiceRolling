6a718cef936ec2e13218c5c9c5ed360bd9e8fe76
David Heizer <DavidHeizer@QuickenLoans.com>
2020-07-20 14:42:57 -0400

    This is my initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceRolling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
